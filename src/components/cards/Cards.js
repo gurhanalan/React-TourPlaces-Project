a69ec45663f157ca554ba1ec4d922e577ef75998
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-09-01 21:15:50 +0300

    Add card module styling 1

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,12 +1,12 @@
 import data from "../../helpers/data";
 
 import React from "react";
-
+import styles from "./cards.module.scss";
 function Cards() {
     return (
-        <div className="cards">
+        <div className={styles.cards}>
             {data.map((item) => (
-                <div className="card">
+                <div className={styles.card}>
                     <h2>{item.title}</h2>
                     <img src={item.image} alt="City" />
                     <p>{item.desc}</p>
