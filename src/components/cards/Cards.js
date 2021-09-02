32eb1d9a010ba2179d9d662d6303fad24952914d
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-09-02 22:49:28 +0300

    Add state to cards to display details or not

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,17 +1,39 @@
 import data from "../../helpers/data";
 
-import React from "react";
+import React, { useState } from "react";
 import styles from "./cards.module.scss";
+
 function Cards() {
+    const [showDetails, setShowDetails] = useState(false);
     return (
-        <div className={styles.cards}>
-            {data.map((item) => (
-                <div className={styles.card}>
-                    <h2>{item.title}</h2>
-                    <img src={item.image} alt="City" />
-                    <p>{item.desc}</p>
-                </div>
-            ))}
+        // <div className={styles.cards}>
+
+        //     {data.map((item) => (
+        //         <div className={styles.card}>
+        //             <h2>{item.title}</h2>
+        //             <img src={item.image} alt="City" />
+        //             <p>{item.desc}</p>
+        //         </div>
+        //     ))}
+        // </div>
+
+        <div
+            className={styles.cards}
+            onClick={() => setShowDetails(!showDetails)}
+        >
+            {showDetails
+                ? data.map((item) => (
+                      <div className={styles.card}>
+                          <h2>{item.title}</h2>
+                          <img src={item.image} alt="City" />
+                          <p>{item.desc}</p>
+                      </div>
+                  ))
+                : data.map((item) => (
+                      <div className={styles.card}>
+                          <img src={item.image} alt="City" />
+                      </div>
+                  ))}
         </div>
     );
 }
